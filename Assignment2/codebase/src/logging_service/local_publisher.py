abcfc34e217a99a9accfd93535ecc07795de0f36
OctCarp <octcarp@hotmail.com>
2024-11-20 17:49:48 +0800

    Move A2 demo code to Assignment2 root

--- Assignment2/codebase/src/logging_service/local_publisher.py
+++ Assignment2/codebase/src/logging_service/local_publisher.py
@@ -14,13 +14,13 @@ topic = 'log-channel'
 
 # produce messages
 try:
-  while True:
-    msg = f'Hello at time = {datetime.now()}'
-    producer.produce(topic, msg.encode('utf-8'))
-    producer.flush()
-    print(msg)
-    time.sleep(1)
+    while True:
+        msg = f'Hello at time = {datetime.now()}'
+        producer.produce(topic, msg.encode('utf-8'))
+        producer.flush()
+        print(msg)
+        time.sleep(1)
 except KeyboardInterrupt:
-  print('\nStopped by keyboard interrupt')
+    print('\nStopped by keyboard interrupt')
 finally:
-  producer.flush()
+    producer.flush()
